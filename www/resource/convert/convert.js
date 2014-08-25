ed181a941890b24a379501eb24eea73688104f28
biin <youngbin.lee@gmail.com>
2014-08-25 09:56:39 +0900

    Update convert.js

--- www/resource/convert/convert.js
+++ www/resource/convert/convert.js
@@ -4,41 +4,29 @@ var bible = require('./bible').bible;
 
 var app = express();
 
-
 var port = process.env.PORT || 8080;
-
-
 
 var fs = require('fs');
 var path = './bible';
-
-
 
 fs.readdir(path, function (err, files) {
     if (err) {
         throw err;
     }
 
-
-    var totalLength = files.length;
-
-
+    //read files
     files.forEach(function(file, i){
-    	
-
 
     	var data = fs.readFileSync(path+'/'+file, "utf8");
     	var bufferString = data.toString(); 
 	    var bufferStringSplit = bufferString.split('\r\n'); 
 	    // console.log(bufferStringSplit);
-	    
-	    
-	    
+
 	    var verses = [];
+
+	    //read lines and take off a number
 	    bufferStringSplit.forEach(function(line, index){
-
 	    	var rgExp = /\d+/;
-
 			var verse = line.replace(rgExp, '').replace(/^\s*|$\s*/g, '');
 			if(verse){
 				verses.push({
@@ -46,19 +34,16 @@ fs.readdir(path, function (err, files) {
 					verse : verse
 				});
 			}
-
 	    });
 
+
 	    var rgExp = /\d+/;
-	    var bookName = file.replace(rgExp, '').replace('.txt', '').replace('개역개정_', '');
-
+	    
+	    // get book name from file name
+	    var bookName = file.replace(rgExp, '').replace('.txt', '').replace('개역개정_', '');
+	    
+	    //make chapter info
 	    var bookInfo = {name : 'test' , abbr : '2'};
-
-
-	    console.log(escape(bookName).charCodeAt())
-	    console.log(escape("창"))
-	    console.log(String.fromCharCode(escape(bookName).charCodeAt()))
-
 	    if(bible[bookName]){
 	    	console.log(bible[bookName])
 	    	bookInfo = bible[bookName];
@@ -72,35 +57,29 @@ fs.readdir(path, function (err, files) {
 	    };
 
 	    
+
 	    var bookPath = './result/' + bookName;
 
 	    // context.book = bookName;
 
-	    var fileName = file.replace('개역개정_', '').replace(bookName, '').replace('.txt', '.json');
-
+	    var fileName = file.replace('개역개정_', '').replace(bookName, '').replace('.txt', '.json');
 
 	    var result = JSON.stringify(context, null, 4);
 
+	    //make file
 	    if(file != '.DS_Store'){
 	    	fs.mkdir(bookPath, function(e){
 			    if(!e || (e && e.code === 'EEXIST')){
 			        //do something with contents
 			        fs.writeFileSync(bookPath+'/'+fileName, result);
-
-
-	    			console.log(Math.ceil((i/totalLength)*100) + '%');
-			        
-
+	    			// console.log(Math.ceil((i/totalLength)*100) + '%');
 			    } else {
 			        //debug
 			        console.log(e);
 			    }
 			});
 	    }
-	    
-   
     });
-
 });
 
 //start app
