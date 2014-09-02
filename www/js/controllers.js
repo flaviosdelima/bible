966e2247c029a6fce1f2f5a340f47ed6fe17b798
youngbin.lee <youngbin.lee@gmail.com>
2014-09-03 01:24:23 +0900

    Add All bible

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,22 +3,42 @@ angular.module('quickbible.controllers', [])
     .controller('HomeCtrl', function ($scope) {
 
         console.log('HomeCtrl');
+
+        $scope.bibleList = [];
+        angular.forEach(bible , function(value, key){
+            $scope.bibleList.push(value);
+        });
+
+        $scope.testBook = $scope.bibleList.slice(0,8);
+
+        $scope.showBook = function(){
+            console.error(arguments)
+        }
     })
 
-    .controller('BibleReadCtrl', function ($scope, $stateParams, $http) {
+    .controller('BibleReadCtrl', function ($scope, $stateParams, $http, $ionicListDelegate) {
         console.log('Bible Read');
         $scope.content = "";
 
         // $http({method: 'GET', url: './resource/test2.txt', headers : {"Content-Type": "text/plain"}})
 
-        $http.get('./resource/1.json')
+        $http.get('./resource/bible/1/1.json')
         .success(function(data, status, headers, config) {
           // this callback will be called asynchronously
           // when the response is available
           $scope.title = data.book;
           $scope.chapter = data.chapter;
           $scope.content = data.verses;
-
+          $scope.sideMenuDeleteStatus = false;
+          $scope.showDeleteButtons = function(){
+                if($scope.sideMenuDeleteStatus){
+                    $scope.sideMenuDeleteStatus = false;
+                    $ionicListDelegate.showDelete(false);
+                }else{
+                    $scope.sideMenuDeleteStatus = true;
+                    $ionicListDelegate.showDelete(true);
+                }
+          }
           
         })
         .error(function(data, status, headers, config) {
